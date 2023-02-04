d71d5965823032a01a95d37cd0fb56d815e99b03
Ayazhan <ayazhan.asylbekova@gmail.com>
2023-02-04 12:35:53 +0600

    Example

---
+++ lab3/tryityourself3/py.classes.objects/4.py
@@ -0,0 +1,13 @@
+"""
+The __str__() function controls what should be returned when the class object is represented as a string.
+
+If the __str__() function is not set, the string representation of the object is returned: 
+"""
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1)
