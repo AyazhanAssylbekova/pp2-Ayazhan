70c9820e62544ea5328d74c0d3adac478138d5a8
Ayazhan <ayazhan.asylbekova@gmail.com>
2023-03-07 22:57:05 +0600

    Example

--- lab6/builtin/41.py
+++ lab6/builtin/41.py
@@ -1,8 +1,9 @@
 import math
 import datetime
+import time
 a=int(input("Enter the square: "))
-c=datetime.datetime.now()
+c=int(input())
 # b=int(input("Enter the msec: "))
 e=math.sqrt(a)
-b=datetime.datetime.now()
-print("Square root of",a,"after",(b-c).seconds,"miliseconds is",e)
+time.sleep(c/1000)
+print(f"Square root of {a} after {c} miliseconds is {e}")
